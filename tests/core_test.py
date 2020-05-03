05ea21bbb2b65a7d9cd7734431b2a2ecd5f78f39
williamwmarx <william@marx.design>
2020-05-03 12:27:03 -0500

    more test debugging

--- tests/core_test.py
+++ tests/core_test.py
@@ -4,17 +4,16 @@
 
 import cloudburst as cb
 
-
 def test_query():
     assert cb.query(".", "jpg") == []
 
-# def is_even(number):
-#     if number%2 == 0:
-#         return 1
-#     else:
-#         return 0
+def is_even(number):
+    if number%2 == 0:
+        return 1
+    else:
+        return 0
 
-# def test_concurrent():
-#     numbers = [0, 5, 12, 57, 8654]      
-#     assert sum(cb.concurrent(is_even, numbers)) == 3
-#     print(result)
+def test_concurrent():
+    numbers = [0, 5, 12, 57, 8654]      
+    assert sum(cb.concurrent(is_even, numbers)) == 3
+    print(result)
