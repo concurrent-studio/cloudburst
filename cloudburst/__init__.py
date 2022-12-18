f6e8bdcf326fe3e1c0acfba2b331f62a124f89c2
William W. Marx <m94@marx.sh>
2022-12-18 17:39:23 -0600

    Retab and clean up

--- cloudburst/__init__.py
+++ cloudburst/__init__.py
@@ -1,24 +1,2 @@
 # -*- coding: utf-8 -*-
-"""
-cloudburst
-==========
-Scrape and analyze data from various platforms
-
-Instagram
----------
-.. autosummary::
-    :toctree: generated/
-
-    Instagram
-    download_instagram_by_shortcode
-
-VSCO
-----
-.. autosummary::
-    :toctree: generated/
-    
-    VSCO
-"""
-
-from .instagram import *
-from .vsco import *
+"""cloudburst"""
