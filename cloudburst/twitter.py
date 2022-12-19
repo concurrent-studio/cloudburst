cdb35fd313227ddd9cb625eb01215ff97c65189f
William W. Marx <m94@marx.sh>
2022-12-19 17:57:56 -0600

    Add method to scrape a user's tweets and replies

--- cloudburst/twitter.py
+++ cloudburst/twitter.py
@@ -79,6 +79,11 @@ class TwitterSession:
 			"x-twitter-client-language": "en"
 		}
 
+	def get_username(self, userId):
+		"""Get a username from a person's userId"""
+		queryURL = f"https://api.twitter.com/1.1/users/lookup.json?user_id={userId}"
+		return requests.get(queryURL, headers=self.headers).json()[0]["screen_name"]
+
 	def user_info(self, username):
 		variables = {
 			'screen_name': username,
@@ -124,3 +129,76 @@ class TwitterSession:
 				"profile_banner_url": result["legacy"].get("profile_banner_url", None),
 				"affiliates": result["affiliates_highlighted_label"].get("label", {}).get("description", None)
 		}
+
+	def get_user_tweets(self, username):
+		"""Get tweets and replies from a user"""
+		user_info = self.user_info(username)
+		variables = {
+			"userId": user_info["id"],
+			"count": 100,
+			"includePromotedContent": True,
+			"withQuickPromoteEligibilityTweetFields": True,
+			"withSuperFollowsUserFields": True,
+			"withDownvotePerspective": False,
+			"withReactionsMetadata": False,
+			"withReactionsPerspective": False,
+			"withSuperFollowsTweetFields": True,
+			"withVoice": True,
+			"withV2Timeline": True
+		}
+		features = {
+			"responsive_web_twitter_blue_verified_badge_is_enabled": True,
+			"verified_phone_label_enabled": False,
+			"responsive_web_graphql_timeline_navigation_enabled": True,
+			"unified_cards_ad_metadata_container_dynamic_card_content_query_enabled": True,
+			"tweetypie_unmention_optimization_enabled": True,
+			"responsive_web_uc_gql_enabled": True,
+			"vibe_api_enabled": True,
+			"responsive_web_edit_tweet_api_enabled": True,
+			"graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+			"standardized_nudges_misinfo": True,
+			"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False,
+			"interactive_text_enabled": True,
+			"responsive_web_text_conversations_enabled": False,
+			"responsive_web_enhance_cards_enabled": True
+		}
+		tweets = []
+		cursors = set()
+		cursor = None
+		while True:
+			variables = {**variables, "cursor": cursor} if cursor else variables
+			query_selection = {
+				'variables': json.dumps(variables).replace(" ", ""),
+				'features': json.dumps(features).replace(" ", "")
+			}
+			tweets_replies_base = "https://twitter.com/i/api/graphql/s0hG9oAmWEYVBqOLJP-TBQ/UserTweetsAndReplies?"
+			tweets_replies_url = tweets_replies_base + urllib.parse.urlencode(query_selection)
+			resp = self.session.get(tweets_replies_url, headers=self.headers).json()
+			for instruction in resp["data"]["user"]["result"]["timeline_v2"]["timeline"]["instructions"]:
+				if instruction["type"] == "TimelineAddEntries":
+					for entry in instruction["entries"]:
+						if entry["entryId"].startswith("cursor-bottom-"):
+							cursor = entry["content"]["value"]
+						elif entry["entryId"].startswith("tweet-"):
+							result = entry["content"]["itemContent"]["tweet_results"]["result"]
+							tweets.append({
+								"id": result["rest_id"],
+								"created_at": result["legacy"]["created_at"],
+								"mentions": [
+									{"name": u["name"], "username": u["screen_name"]}
+									for u in result["legacy"]["entities"]["user_mentions"]
+								],
+								"urls": result["legacy"]["entities"]["urls"],
+								"hashtags": result["legacy"]["entities"]["hashtags"],
+								"symbols": result["legacy"]["entities"]["symbols"],
+								"likes_count": result["legacy"]["favorite_count"],
+								"quote_count": result["legacy"]["quote_count"],
+								"reply_count": result["legacy"]["reply_count"],
+								"retweet_count": result["legacy"]["retweet_count"],
+								"retweeted": result["legacy"]["retweeted"],
+								"source": result["legacy"]["source"]
+							})
+			if cursor in cursors:
+				break
+			cursors.add(cursor)
+		return tweets
